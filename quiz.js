0202a5bb49486d38d59106087154f6416f92a320
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-01-22 12:35:14 +0100

    everything is working fine

--- quiz.js
+++ quiz.js
@@ -1,1 +1,26 @@
+let userAnswer = document.querySelector('input[name="quiz"]');
+let feedBack = document.querySelector('#feedback');
+//console.log(feedBack)
+//console.log(userAnswer.value)
 
+//feedBack.innerHTML = 'wrong'
+function checkAnswer(){
+    let correctAnswer = 4;
+
+    if(userAnswer.value==correctAnswer){
+        feedBack.innerHTML = "Correct! well done"
+        feedBack.style.color = 'green'
+
+    }else{
+        feedBack.innerHTML = "That's incorrect. try again! "
+        feedBack.style.color = 'red'
+    }
+}
+  
+//checkAnswer();
+
+//EVENT LISTENER
+let btn = document.getElementById('submit-answer')
+//console.log(btn)
+
+btn.addEventListener('click', checkAnswer);
